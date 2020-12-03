01bfed0e852d665fc587f1bc01e9ef6422833585
Carlos de la Cruz <carlosdelacruzacosta4@gmail.com>
2020-12-03 13:40:49 -0600

    hide icons

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const projects = document.querySelectorAll(".project");
+const wrapper = document.querySelector("#wrapper");
 
 projects.forEach(project => project.addEventListener("mouseover", (e) => displayIcons(e)));
-
+projects.forEach(project => project.addEventListener("mouseout", (e) => hideIcons(e)));
 function  displayIcons(e){
-   // console.log(e.target.children[0].classList.add("active"));
-   e.target.children[0].classList.add("active")
+   e.target.children[0].classList.add("active");
 }
+
+
+function hideIcons (e){
+    e.target.children[0].classList.remove("active");
+}
+// function hideIcons () {
+//    document.querySelectorAll(".icons").forEach(setIcons => setIcons.classList.remove("active"));
+// }
